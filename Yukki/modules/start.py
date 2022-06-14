e3cc94af71854c83112d1e3aee8f8f15f5c16bd4
Ishu <87901742+Ishu-Hinata@users.noreply.github.com>
2022-06-14 22:17:44 +0530

    Update start.py

--- Yukki/modules/start.py
+++ Yukki/modules/start.py
@@ -46,11 +46,12 @@ async def run(client, message):
     await app.send_sticker(message.chat.id,"CAACAgEAAxkBAAFC0AFiqLFq96XquupWkC3Wjww8cIo8HwACRAIAAtSXSEXU4M20BCBnFCQE")
     await message.reply_text(f"Now look at you \n There's No One Ugly as You 😝")
     await app.send_sticker(message.chat.id,"CAACAgEAAxkBAAFC0FZiqLivDE15hr0iUXXB3uLKkm4iGQACvQIAAm9fSEXTjPhY1VGe5SQE")
-    await message.reply_text(f"I Really Think You should be in Zoo 😂")
+    await message.reply_text(f"I Really Think You should be in the Zoo 😂")
     await app.send_sticker(message.chat.id,"CAACAgEAAxkBAAFC0FliqLkzC0-vr0HKeCxg-QfbH8IW0gACbgIAAgnDSEVqYh91csOLqyQE")
-    await message.reply_text(f"")
-    await app.send_sticker(message.chat.id,"
-    await message.reply_text(f"")
-    await app.send_sticker(message.chat.id,"
-    await message.reply_text(f"")
-    await app.send_sticker(message.chat.id,"
+    await message.reply_text(f"Don't worry I'll be there too 🥺")
+    await app.send_sticker(message.chat.id,"CAACAgEAAxkBAAFC0F9iqLoP10v9cGFlNDYlvMkf5EJcHgACDgIAApwgSUXoPrEi4_q4tyQE")
+    await message.reply_text(f"But Laughing at you!!! ")
+    await app.send_sticker(message.chat.id,"CAACAgUAAxkBAAFC0GJiqLqM2c0OK0MM45QaNtMwYlpU9AACuwUAAg2iqVdfW2GdjZSKYSQE")
+    await message.reply_text(f"Ask for repo again And \n I will hit you with a shoe 🌝")
+    await app.send_sticker(message.chat.id,"CAACAgEAAxkBAAFC0GZiqLubtDfUiSZOCibf8BS7LzsnuwACSgMAAlopSEUBme_jF0ul2yQE")
+    
