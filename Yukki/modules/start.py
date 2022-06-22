f7de6dd1568ca4135ef2a431b0cc5aa87be2aa3c
DSP <107468577+Lord-DSP-3@users.noreply.github.com>
2022-06-22 20:06:18 +0530

    Update start.py

--- Yukki/modules/start.py
+++ Yukki/modules/start.py
@@ -15,32 +15,9 @@ async def on_start(_, message: Message):
     )
     await app.send_sticker(message.chat.id,"CAACAgEAAxkBAAFCz7xiqKSb-T2ZG9im-K7pMdlJ_kZSmQACNwIAAsuOSEX5bh5-Omr7jSQE")
 
-@app.on_message(filters.command(["settings"])& filters.private)
-async def run(client, message):
-    await message.reply_text(f"Only For Authorised Users")
-
-
-@app.on_message(filters.command(["help"]))
-async def run(client, message):
-    await message.reply_text(f"⛑️Promote me as Admin in group.\n⌱Only 'delete messesge' rights required.\n\n💚Usage of Afk command:\n/afk \n/afk Reason \n/afk (reply to a sticker) \n/afk (reply to a sticker) Reason \n/afk (reply to a image) \n/afk (reply to a image) Reason\n\n💛Others:\n/ping  - To see bot Uptime \n/repo  - Source code \n\n\n📜 ⌱ UCO PROJECT")
-    await app.send_sticker(message.chat.id,"CAACAgEAAxkBAAFCz-hiqK7ULTKZTWvsw5lVTf83G-ielgACbgIAAgnDSEVqYh91csOLqyQE")
-
-@app.on_message(filters.command(["scan"])& filters.private)
-async def run(client, message):
-    await message.reply_text(f"Only For Authorised Users")
 
 @app.on_message(filters.command(["repo"])& filters.private)
 async def run(client, message):
     await asyncio.sleep(3)
     await message.delete()
     await message.reply_text(f"Look at sky \n Sky is Blue 💙")
-
-
-@app.on_message(filters.command(["ping"]))
-async def on_start(_, message: Message):
-    bot_uptime = int(time.time() - boot)
-    Uptime = get_readable_time(bot_uptime)
-    await message.reply_text(
-        f"{botname} is alive and working good.\n\nUptime : {Uptime}"
-    )
-    await app.send_sticker(message.chat.id,"CAACAgEAAxkBAAFC3hNiqaNlV1mevOdOu1jDjAsSRWgBGQACtwIAAuYuSEV3tv6QBNjZvSQE")
